7afb67de115f50e26fb1b1b39a04a00a68bdd3c3
Mihai Bargau <mihaibargau@gmail.com>
2026-09-07 22:26:39 +0300

    lab 03-foundry-sdk use Responses API

--- labfiles/foundry-chat/python/chat-app/chat-app.py
+++ labfiles/foundry-chat/python/chat-app/chat-app.py
@@ -33,17 +33,23 @@ def main():
                 continue
      
             # Get a response
-            completion = openai_client.chat.completions.create(
+            # completion = openai_client.chat.completions.create(
+            #     model=model_deployment,
+            #     messages=[
+            #         {
+            #             "role": "system",
+            #             "content": "You are a helpful AI assistant that answers questions and provides information.",
+            #         },
+            #         {"role": "user", "content": input_text},
+            #     ],
+            # )
+            # print(completion.choices[0].message.content)
+            response = openai_client.responses.create(
                 model=model_deployment,
-                messages=[
-                    {
-                        "role": "system",
-                        "content": "You are a helpful AI assistant that answers questions and provides information.",
-                    },
-                    {"role": "user", "content": input_text},
-                ],
+                 instructions="You are a helpful AI assistant that answers questions and provides information.",
+                 input=input_text
             )
-            print(completion.choices[0].message.content)
+            print(response.output_text)
 
     except Exception as ex:
         print(ex)
